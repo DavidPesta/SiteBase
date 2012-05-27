2e674fc3d79c72f1cecac2d47173cd51c5e7e036
David Pesta <davidpesta@gmail.com>
2012-05-26 19:30:28 -0500

    Added text for the MIT license that this software is licensed under.

--- config.php
+++ config.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+* Copyright (c) 2012 David Pesta, https://github.com/DavidPesta/SiteBase
+* Licensed under the MIT License.
+* You should have received a copy of the MIT License along with this program.
+* If not, see http://www.opensource.org/licenses/mit-license.php
+*/
+
+
 // Define constants
 
 define( "PRODUCTION", false );
