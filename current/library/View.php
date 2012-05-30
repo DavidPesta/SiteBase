8c2720a76398d79f570b199991c5f38c645d1a28
David Pesta <davidpesta@gmail.com>
2012-05-30 08:25:26 -0500

    Adding $this->image( "path/to/image/from/source" ) in any of the phtml files will base64 those images into CSS files, it will include those CSS files wherever $this->head() is invoked (in the document head), and it will display those images as divs on the page with standard image behavior.

--- current/library/View.php
+++ current/library/View.php
@@ -11,6 +11,10 @@ class View
 {
 	protected $_pageView = null;
 	protected $_components = null;
+	protected $_curViewTag = null;
+	protected $_images = array();
+	protected $_viewTagAssetHashes = null;
+	protected $_document = null;
 	
 	public function __construct( $pageView = null, $components = null )
 	{
@@ -41,17 +45,113 @@ class View
 	{
 		ob_start( 'ob_gzhandler' );
 		
+		ob_start();
+		
 		if( ! is_array( $this->_pageView ) ) throw new Exception( "You must call addPageView() for your view object to configure page view data" );
-		elseif( isset( $this->_pageView[ 'layout' ] ) ) include SOURCE . "/" . $this->_pageView[ 'layout' ];
-		elseif( isset( $this->_pageView[ 'content' ] ) ) include SOURCE . "/" . $this->_pageView[ 'content' ];
+		elseif( isset( $this->_pageView[ 'layout' ] ) ) $this->content( 'layout' );
+		elseif( isset( $this->_pageView[ 'content' ] ) ) $this->content( 'content' );
 		else throw new Exception( "Page view data is not configured properly in your view object" );
+		
+		$this->_document = ob_get_contents();
+		
+		ob_clean();
+		
+		$this->buildCache();
+		$this->renderHead();
+		
+		echo $this->_document;
 		
 		ob_end_flush();
 	}
 	
 	public function content( $viewTag )
 	{
+		$prevViewTag = $this->_curViewTag;
+		$this->_curViewTag = $viewTag;
+		
+		if( ! is_array( $this->_images[ $this->_curViewTag ] ) ) $this->_images[ $this->_curViewTag ] = array();
+		
 		if( isset( $this->_pageView[ $viewTag ] ) ) include SOURCE . "/" . $this->_pageView[ $viewTag ];
 		else throw new Exception( "File tag '" . $viewTag . "' not found in page view data" );
+		
+		$this->_curViewTag = $prevViewTag;
+	}
+	
+	public function image( $image, $style = "default" )
+	{
+		$this->_images[ $this->_curViewTag ][ $image ] = $image; // Using $image as the array key prevents redundancy
+		
+		// We can add multiple different kinds of $style presets here, where "default" is the default preset
+		if( "default" == $style ) $style = " style=\"display: inline-block;\""; // This style represents standard image behavior
+		else $style = " style=\"" . $style . "\"";
+		
+		return "<div" . $style . " class=\"" . $this->_curViewTag . "-image-" . md5( $image ) . "\"></div>";
+	}
+	
+	public function head()
+	{
+		return "<INSERT_HEAD_DATA>";
+	}
+	
+	protected function buildCache()
+	{
+		$viewTagAssetHashes = $this->getViewTagAssetHashes();
+		
+		foreach( $viewTagAssetHashes as $viewTag => $hash ) {
+			$filename = WEBROOT . "/css/" . $viewTag . "-" . $hash . ".css";
+			
+			if( ! is_file( $filename ) ) {
+				$css = "";
+				
+				foreach( $this->_images[ $viewTag ] as $image ) {
+					$imgData = getimagesize( SOURCE . "/" . $image );
+					
+					$rawImage = file_get_contents( SOURCE . "/" . $image );
+					$base64 = base64_encode( $rawImage );
+					
+					$css .= "
+						." . $viewTag . "-image-" . md5( $image ) . " {
+							width: " . $imgData[ 0 ] . "px;
+							height: " . $imgData[ 1 ] . "px;
+							background-image: url(\"data:" . $imgData[ 'mime' ] . ";base64," . $base64 . "\");
+						}
+					";
+				}
+				
+				file_put_contents( $filename, $css );
+			}
+		}
+	}
+	
+	protected function getViewTagAssetHashes()
+	{
+		if( null == $this->_viewTagAssetHashes ) {
+			$viewTagAssetHashes = array();
+			
+			foreach( $this->_images as $viewTag => $images ) {
+				ksort( $images );
+				if( ! empty( $images ) ) $viewTagAssetHashes[ $viewTag ] = md5( implode( ":", $images ) );
+			}
+			
+			$this->_viewTagAssetHashes = $viewTagAssetHashes;
+			
+			return $viewTagAssetHashes;
+		}
+		else {
+			return $this->_viewTagAssetHashes;
+		}
+	}
+	
+	protected function renderHead()
+	{
+		$viewTagAssetHashes = $this->getViewTagAssetHashes();
+		
+		$head = "";
+		
+		foreach( $viewTagAssetHashes as $viewTag => $hash ) {
+			$head .= "<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/" . $viewTag . "-" . $hash . ".css\" />\n";
+		}
+		
+		$this->_document = str_replace( "<INSERT_HEAD_DATA>", $head, $this->_document );
 	}
 }
